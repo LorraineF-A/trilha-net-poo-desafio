326d3b1e7dab5ef9e4044cb78e2fb1a3d983babb
LorraineF-A <lorrainef39@gamil.com>
2024-10-30 21:53:05 -0300

    add files

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,23 +2,36 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; set; }    
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        // TODO: Passar os parâmetros do construtor para as propriedades
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo; 
+            IMEI = imei; 
+            Memoria = Memoria; 
         }
 
-        public void Ligar()
+        protected Smartphone(string numero)
         {
-            Console.WriteLine("Ligando...");
+            Numero = numero;
         }
+
+        public void Ligar() => Console.WriteLine("Ligando...");
 
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+        public string NomeModeloCompleto()
+        {
+            return $"MODEL: {Modelo} / MEMORIA: {Memoria}Gb / IMEI: {IMEI}";
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Nokia nok1 = new Nokia("(21)3333-8765", "Nokia-TijoloMASTER", "587889988-98", 4);
+
+        nok1.InstalarAplicativo("XP INVESTIMENTOS");
+
+        Console.WriteLine("\n");
+
+        Iphone ipho1 = new Iphone(numero: "(21)11111-9898",
+                                "Iphone-$FacadaMaster",
+                                "6548644987-98",
+                                3);
+        ipho1.InstalarAplicativo("WhatsApp");
+    }
+}
